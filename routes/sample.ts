25c48919abd6251ee6969cdf836c09bfdf51da87
snehasaraaby <48785003+snehasaraaby@users.noreply.github.com>
2021-05-13 11:54:38 +0530

    Nodejs API tutorial

---
+++ routes/sample.ts
@@ -0,0 +1,7 @@
+import express, { Router } from "express";
+import controller from "../controllers/sample";
+
+const router = express.Router();
+
+router.get("/ping", controller.sampleHealthCheck);
+export = router;
